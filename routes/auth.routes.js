f8df37c56be738e1cc5992ccaa6d4f18acb96e0a
Stanley Fils <stanleyfils@gmail.com>
2020-04-05 22:14:21 -0400

    Add route files

---
+++ routes/auth.routes.js
@@ -0,0 +1,49 @@
+// routes/auth.routes.js
+
+const { Router } = require('express');
+const router = new Router();
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
+const User = require('../models/User.model');
+
+// .get() route ==> to display the signup form to users
+router.get('/signup', (req, res) => res.render('auth/signup'));
+
+// .post() route ==> to process form data
+router.post('/signup', (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  bcryptjs
+    .genSalt(saltRounds)
+    .then(salt => bcryptjs.hash(password, salt))
+    .then(hashedPassword => {
+      return User.create({
+        // username: username
+        username,
+        email,
+        // passwordHash => this is the key from the User model
+        //     ^
+        //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
+        passwordHash: hashedPassword
+      });
+    })
+    .then(userFromDB => {
+      console.log('Newly created user is: ', userFromDB);
+      res.redirect('/userProfile');
+    })
+    .catch(error => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).render('auth/signup', { errorMessage: error.message });
+      } else if (error.code === 11000) {
+        res.status(500).render('auth/signup', { 
+           errorMessage: 'Username and email need to be unique. Either username or email is already used.' 
+        });
+      } else {
+        next(error)
+      }
+    });
+});
+
+router.get('/userProfile', (req, res) => res.render('users/user-profile'));
+
+module.exports = router;
